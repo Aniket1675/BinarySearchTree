39b1d141c0796389d6f09862e77ec8e3643e4c5a
Aniket1675 <Aniket@05>
2022-03-29 00:33:10 +0530

    added the inoder method of tree

--- src/com/bridgelabz/datastructure/BinaryTreeClass.java
+++ src/com/bridgelabz/datastructure/BinaryTreeClass.java
@@ -39,6 +39,17 @@ public class BinaryTreeClass {
         preorder(root.right);
     }
 
+    public static void inorder(Node root)
+    {
+        if(root == null)
+        {
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.data + "  ");
+        inorder(root.right);
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to Binary Search Tree program");
         int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
@@ -50,6 +61,10 @@ public class BinaryTreeClass {
         System.out.println("==================================");
         System.out.println("Display Preorder");
         preorder(root);
+        System.out.println();
+        System.out.println("==================================");
+        System.out.println("Display Inorder");
+        inorder(root);
         System.out.println();
         System.out.println("==================================");
 
