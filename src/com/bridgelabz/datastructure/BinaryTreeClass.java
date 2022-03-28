58e2d0684fd0422ef2026925c57ab9abd6028d04
Aniket1675 <Aniket@05>
2022-03-29 00:20:49 +0530

    welcome to Binary Search Tree program

---
+++ src/com/bridgelabz/datastructure/BinaryTreeClass.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.datastructure;
+
+public class BinaryTreeClass {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Binary Search Tree program");
+    }
+}
